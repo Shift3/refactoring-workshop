2cf4338ef2f19c4222908481c925e6462f68d265
Justin Schiff <AmbientRevolution@gmail.com>
2019-12-18 17:30:13 -0800

    jslides.

--- 01-extract-method/order-printer.js
+++ 01-extract-method/order-printer.js
@@ -1,14 +1,4 @@
 class OrderPrinter {
-  get ITEM_COST() {
-    return {
-      'Macbook Pro': 2000,
-      'ThinkPad': 150,
-      'Monitor Stand': 999
-    };
-  }
-
-  get TAX() { return 0.095; }
-
   constructor(items, output = console) {
     this.items = items;
     this.output = output;
@@ -19,7 +9,11 @@ class OrderPrinter {
     this.output.log('--------------');
 
     let subtotal = this.items.reduce((sum, item) => {
-      let cost = this.ITEM_COST[item];
+      let cost = {
+        'Macbook Pro': 2000,
+        'ThinkPad': 150,
+        'Monitor Stand': 999
+      }[item];
       this.output.log(`${item}: ${cost.toFixed(2)}`);
 
       return sum + cost;
@@ -27,9 +21,9 @@ class OrderPrinter {
 
     this.output.log('--------------');
     this.output.log(`Subtotal: ${subtotal.toFixed(2)}`);
-    this.output.log(`Tax: ${(subtotal * this.TAX).toFixed(2)}`);
+    this.output.log(`Tax: ${(subtotal * 0.095).toFixed(2)}`);
     this.output.log('--------------');
-    this.output.log(`Total: ${(subtotal + (subtotal * this.TAX)).toFixed(2)}`);
+    this.output.log(`Total: ${(subtotal + (subtotal * 0.095)).toFixed(2)}`);
   }
 }
 
